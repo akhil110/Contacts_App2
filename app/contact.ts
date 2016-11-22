8f9dbe65323bb8d6f1f7fcc0a19e3256a9543a4e
Akhilesh Srivastava <akhil110@gmail.com>
2016-11-21 23:06:23 -0700

    Create contact.ts

---
+++ app/contact.ts
@@ -0,0 +1,12 @@
+export interface IContact {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    add1: string;
+    add2: string;
+    city: string;
+    state: string;
+    country: string;
+    zip: string;
+}
