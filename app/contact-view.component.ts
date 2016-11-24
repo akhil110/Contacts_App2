35a980572a2ebf61e31126b2ed5cd66934f0de68
Akhilesh Srivastava <akhil110@gmail.com>
2016-11-23 19:55:18 -0700

    Update contact-view.component.ts

--- app/contact-view.component.ts
+++ app/contact-view.component.ts
@@ -1,30 +1,42 @@
-import { Component, OnInit }  from '@angular/core';
-import { RouteParams, Router } from '@angular/router-deprecated';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription }       from 'rxjs/Subscription';
+
 import { ContactService } from './contact.service';
 
 @Component({
     templateUrl: 'app/contact-view.component.html'
 })
-export class ContactViewComponent implements OnInit {
+export class ContactViewComponent implements OnInit, OnDestroy {
     pageTitle: string = 'View contact';
     errorMessage: string;
     contacts: any;
+    private sub: Subscription;
    
  
-    constructor( private _router: Router, private _routeParams: RouteParams, private _contactService: ContactService){
+    constructor( private _route: ActivatedRoute,
+        private _router: Router,
+        private _contactService: ContactService){
     }
 
     ngOnInit():void{
-        let id = this._routeParams.get('id');
-        this._contactService.getContact(id)
-            .subscribe(
-                contacts => this.contacts = contacts,
-                error => this.errorMessage = <any>error
-            );
+        this.sub = this._route.params.subscribe(
+            params => {
+                let id = +params['id'];
+                this._contactService.getContact(id)
+                .subscribe(
+                    contacts => this.contacts = contacts,
+                    error => this.errorMessage = <any>error
+                );
+        });
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
     }
 
     onBack(): void {
-        this._router.navigate(['AllContacts']);
+        this._router.navigate(['allcontacts']);
     }
 
 }
